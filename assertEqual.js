41b65ee11e8d72ff174853991a5efcbc17763e8c
jackymomomo <jacklloydrobinson@gmail.com>
2022-11-16 04:57:19 -0800

    starting function of project look back for example

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,5 @@
+
+/*
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -13,3 +15,5 @@ assertEqual(1, 1);
 assertEqual('banana', 'banana');
 assertEqual('buns', 'bananas');
 assertEqual(2672, 2672);
+*/
+
