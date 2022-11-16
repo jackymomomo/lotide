0fd50cfd85e366ac46707bd66988f494d42efc25
jackymomomo <jacklloydrobinson@gmail.com>
2022-11-16 04:58:27 -0800

    only the last part of our array we want to print the tail of our function

---
+++ tail.js
@@ -0,0 +1,26 @@
+// const _ = require('lodash');
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+ 
+};
+const head = function(firstEl) {
+  return firstEl[0];
+};
+module.exports = head;
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+module.exports = tail;
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
